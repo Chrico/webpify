020f97fcf5e87fa74c79802b3f972cf0876593bd
ChriCo <family.brueckner@googlemail.com>
2017-09-05 15:50:00 +0200

    moved from vanilla-LazyLoad version 10.x back to 8.1.x, because the Intersection Observer API is currently not widely enough supported.

--- TODO.md
+++ TODO.md
@@ -1,9 +1,13 @@
 # TODO
 
+## JavaScript
+* `vanilla-lazyload` supports with version 10.x the [Intersection Observer API](https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API).
 
-* Configurations
-  * Which transformer
-  * Quality of WebP image.
+## Configuration
+* Which transformer to use
+* Quality of WebP image.
+
+## Stats
 * More info about state in...
   * `/wp-admin/upload.php` (new column)
   * "Media Edit"-Modal (image detail)
--- assets/js/src/WebPify.js
+++ assets/js/src/WebPify.js
@@ -1,41 +1,49 @@
 /*! global WebPify */
 import LazyLoad from 'vanilla-lazyload';
 
-// detect if a image has the "data-web-src"- or "data-web-srcset"-attributes
-// and replace the default "data-src" and/or "data-srcset"-attributes with it.
-const maybeReplaceDefaultImages = () => {
-	[].forEach.call(
-		document.querySelectorAll( 'img' ),
-		$el => {
-			const attributes = [
-				{
-					'to'  : 'data-src',
-					'from': 'data-webp-src'
-				},
-				{
-					'to'  : 'data-srcset',
-					'from': 'data-webp-srcset'
-				}
-			];
-			attributes.forEach( search => {
-				if ( $el.hasAttribute( search.from ) ) {
-					$el.setAttribute( search.to, $el.getAttribute( search.from ) );
-					$el.removeAttribute( search.from );
-				}
-			} );
-		}
-	);
+if ( typeof window.CustomEvent !== "function" ) {
+
+	function CustomEvent( event, params ) {
+		params = params || { bubbles: false, cancelable: false, detail: undefined };
+		let evt = document.createEvent( "CustomEvent" );
+		evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+		return evt;
+	}
+
+	CustomEvent.prototype = window.Event.prototype;
+	window.CustomEvent = CustomEvent;
+}
+
+const replaceAttribute = ( $el, from, to ) => {
+	if ( !$el.hasAttribute( from ) ) {
+		return false;
+	}
+	$el.setAttribute( to, $el.getAttribute( from ) );
+	$el.removeAttribute( from );
+	return true;
 };
 
-const initializeLazyLoad = () => {
+// detect if a image has the "data-web-src"- or "data-web-srcset"-attributes
+// and replace the default "data-src" and/or "data-srcset"-attributes with it.
+const setWebPIfExists = ( cb ) => {
+	[].forEach.call(
+		document.querySelectorAll( WebPify.options.elements_selector ),
+		$el => {
+			replaceAttribute( $el, 'data-webp-src', 'data-src' );
+			replaceAttribute( $el, 'data-webp-srcset', 'data-srcset' );
+		}
+	);
+	cb();
+};
 
+// create instance of LazyLoad.
+const initializeLazyLoad = () => {
 	new LazyLoad( WebPify.options || {} );
 };
 
 const webp = new Image();
 webp.onerror = initializeLazyLoad;
 webp.onload = function() {
-	maybeReplaceDefaultImages();
-	initializeLazyLoad();
+	setWebPIfExists( initializeLazyLoad );
 };
 webp.src = 'data:image/webp;base64,UklGRiQAAABXRUJQVlA4IBgAAAAwAQCdASoBAAEAAwA0JaQAA3AA/vuUAAA=';
