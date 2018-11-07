8e35cdd94aa4c6abf2d7eec4cbf341b39a7e60d3
Chris <Chrico@users.noreply.github.com>
2018-11-07 15:31:39 +0100

    version 1.1.1

--- webpify.php
+++ webpify.php
@@ -4,7 +4,7 @@
  * Description:       Generating automatically WebP-images and provide them via feature detection and lazy loading.
  * Author:            ChriCo
  * Author URI:        https://www.chrico.info
- * Version:           1.1
+ * Version:           1.1.1
  * Text Domain:       webpify
  * Domain Path:       languages
  * License:           MIT
